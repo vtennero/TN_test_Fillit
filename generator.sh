07084ceb8c48458302c4a46d4e8a0e65b16c8cfa
Victor TENNERONI <vtennero@student.42.fr>
2017-12-04 18:14:18 +0100

    interface update

--- generator.sh
+++ generator.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 c=1
 n=$2
+d=$3
+
+COLOR='\x1b[38;5;87m'
+END='\x1b[0m'
 /bin/rm -f tests/*
 while [ $c -le $n ]
 do
 	/bin/rm -f Tetriminos-generator/sample.fillit
 	/bin/rm -f sample.fillit
-	let "r =  $(( ( RANDOM % 10 )  + 1 ))"
+	let "r =  $(( ( RANDOM % $d )  + 1 ))"
 	Tetriminos-generator/./tetri-gen $1 -f $r
-	#if [ $c -ge 2 ]; then
-	#/bin/rm -f tests/*
-#fi
 	cp sample.fillit tests/test$c-$r
-		echo "test$c : $r tetris" && ./fillit "tests/test$c-$r"
+	echo "$COLOR\0test$c : $r tetris$END" && ./fillit "tests/test$c-$r"
 		let "c++"
 done
 
